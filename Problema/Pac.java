fe191d0851514c817af8c4ac549baf36039c67ed
Aline Sabel <131200331+line127@users.noreply.github.com>
2025-02-23 17:36:42 -0300

    Delete Problema/Pac.java

--- Problema/Pac.java
+++
@@ -1,8 +0,0 @@
-public class Pac implements TipoFrete{
-
-    @Override
-    public double calcularFrete(double peso) {
-        throw new UnsupportedOperationException("Unimplemented method 'calcularFrete'");
-    }
-
-}
